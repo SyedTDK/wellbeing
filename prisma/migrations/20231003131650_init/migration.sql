55e2274493537d51026d11799ccddccd5db7d38a
SyedTDK <syedfaizullah123@gmail.com>
2024-01-24 23:48:28 -0500

    bugs

--- prisma/migrations/20231003131650_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Reflection" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "title" TEXT NOT NULL,
-    "complete" BOOLEAN NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL
-);
