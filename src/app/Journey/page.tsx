b5f6f23e0187d0e02bc5c9622964ed110d5de199
SyedTDK <syedfaizullah123@gmail.com>
2024-01-27 13:44:28 -0500

    Update page.tsx

--- src/app/Journey/page.tsx
+++ src/app/Journey/page.tsx
@@ -87,11 +87,11 @@ const Journey: React.FC = () => {
     //If the user is not logged in, display the page with a link to the login page.
     } else { 
         return (
-            <body className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+            <body className="w-full h-screen bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
                 <header>
                     <div className="content py-2">
                         <nav className="flex items-center justify-between">
-                            <h2 className="text-gray-200 font-bold text-lg sm:text-4xl">Well Being</h2>
+                            <h2 className="text-gray-200 font-bold text-lg sm:text-4xl">WellBeing</h2>
                             <div className="auth flex items-center">
                                 <Link className="bg-gray-200 text-gray-900  p-2 rounded border border-gray-300 mr-4 hover:bg-gray-100 hover:text-gray-700" href="/auth/login">Log in</Link>
                                 <Link className="bg-gray-900 text-gray-200  py-2 px-3 rounded  hover:bg-gray-800 hover:text-gray-100" href="/auth/register">Sign up</Link>
@@ -99,7 +99,7 @@ const Journey: React.FC = () => {
                         </nav>
                     </div> 
                 </header>  
-                <div className="w-screen h-screen flex justify-center items-center">
+                <div className="w-full h-screen flex justify-center items-center">
                     <Link className="text-center font-bold text-5xl underline decoration-sky-600 hover:decoration-blue-400" href='../auth/login'>Get access by logging in for free!</Link>
                 </div>
             </body>
