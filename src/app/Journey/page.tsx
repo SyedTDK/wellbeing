30e6a0fe0823166b52bdd67c9e52f7a13dbd7396
SyedTDK <syedfaizullah123@gmail.com>
2024-02-14 21:57:23 -0500

    Added footer

--- src/app/Journey/page.tsx
+++ src/app/Journey/page.tsx
@@ -85,6 +85,26 @@ const Journey: React.FC = () => {
                     <Link href="#" ></Link>
                     <div className="text-black">{message}</div>
                 </div>
+                <footer className="bg-gray-800 rounded-lg shadow m-4">
+                    <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+                        <span className="text-sm sm:text-center text-gray-400">© 2023 WellBeing™. All Rights Reserved.
+                        </span>
+                        <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-400 sm:mt-0">
+                            <li>
+                                <a href="#" className="hover:underline me-4 md:me-6">About</a>
+                            </li>
+                            <li>
+                                <a href="#" className="hover:underline me-4 md:me-6">Privacy Policy</a>
+                            </li>
+                            <li>
+                                <a href="#" className="hover:underline me-4 md:me-6">Licensing</a>
+                            </li>
+                            <li>
+                                <a href="#" className="hover:underline">Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+                </footer>
             </body>   
         </>
 
@@ -106,6 +126,26 @@ const Journey: React.FC = () => {
                 <div className="w-full h-screen flex justify-center items-center">
                     <Link className="text-center font-bold text-white text-5xl underline decoration-sky-600 hover:decoration-blue-400" href='../auth/login'>Get access by logging in for free!</Link>
                 </div>
+                <footer className="bg-gray-800 rounded-lg shadow m-4">
+                    <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+                        <span className="text-sm sm:text-center text-gray-400">© 2023 WellBeing™. All Rights Reserved.
+                        </span>
+                        <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-400 sm:mt-0">
+                            <li>
+                                <a href="#" className="hover:underline me-4 md:me-6">About</a>
+                            </li>
+                            <li>
+                                <a href="#" className="hover:underline me-4 md:me-6">Privacy Policy</a>
+                            </li>
+                            <li>
+                                <a href="#" className="hover:underline me-4 md:me-6">Licensing</a>
+                            </li>
+                            <li>
+                                <a href="#" className="hover:underline">Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+                </footer>
             </body>
         )}
 }
