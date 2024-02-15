6769d7272abf1efaa78268424fe8fb6d5df349b6
SyedTDK <syedfaizullah123@gmail.com>
2024-02-14 23:50:03 -0500

    Update Footer.tsx

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
     return (
         <footer className="bg-gray-800 rounded-lg shadow m-4">
             <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-                <span className="text-sm sm:text-center text-gray-400">© 2023 WellBeing™. All Rights Reserved.
+                <span className="text-sm sm:text-center text-gray-400">© 2024 WellBeing™. All Rights Reserved.
                 </span>
                 <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-400 sm:mt-0">
                     <li>
