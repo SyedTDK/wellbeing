3c49dbfe2d36a8925854bdd955aa68418828c1bc
SyedTDK <syedfaizullah123@gmail.com>
2024-06-18 19:04:14 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -51,7 +51,7 @@ export default async function Home() {
           <p className="mb-8 text-lg font-normal  lg:text-xl sm:px-16 xl:px-48 ">The journey of your mental well being starts here. Perform meditations, post and reflect on thoughtful questions about life and the universe.</p>
           <div className="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
               <a href="/auth/register" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-indigo-600 bg-gradient-to-br from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#08f,0_0_15px_#08f,0_0_30px_#08f]">
-                  Get Started
+                  Begin your journey
                   <svg className="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
               </a>
           </div>  
